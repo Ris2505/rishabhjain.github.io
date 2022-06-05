9423ad251372d2f692f061a5cd74982e80d34d41
Ris2505 <91727761+Ris2505@users.noreply.github.com>
2022-06-05 20:11:34 +0530

    Add files via upload

---
+++ send_form_email1.php
@@ -0,0 +1,29 @@
+<?php
+
+// Details
+echo $_POST['name'];
+echo "<br>";
+echo $_POST['mobile'];
+echo "<br>";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['subjects'];
+echo "<br>";
+echo $_POST['message'];
+echo "<br>";
+
+?>
+
+ 
+
+ 
+
+ 
+
+
+ 
+
+ 
+
+ 
+
